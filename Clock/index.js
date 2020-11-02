fc646d16652d026418282e9e21e22519e5ef98f6
Emmad Usmani <emmadausmani@gmail.com>
2020-11-02 13:59:25 -0800

    simplified html (and code restyled with prettier)

---
+++ Clock/index.js
@@ -0,0 +1,10 @@
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  time = h + ":" + m + ":" + s;
+  document.getElementById("clock").innerText = time;
+}
+showTime();
+setInterval(showTime, 1000);
